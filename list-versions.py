0bb04f9d4b0d178d569593b31f854eb50e91a17a
Zoe Levesanou <zoeleve4@hotmail.gr>
2024-02-13 17:06:01 +0200

    Update list-versions.py

--- list-versions.py
+++ list-versions.py
@@ -22,6 +22,7 @@ def get_package_versions(owner, repo, package_name):
     }
 
     response = requests.get(api_url, headers=headers)
+    print(response.status_code)
     if response.status_code == 200:
         versions = response.json()
         if versions:
