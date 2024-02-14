fe6c60f9dfa1cfbfe117f87ec2b92247b18cf726
Zoe Levesanou <zoeleve4@hotmail.gr>
2024-02-14 10:54:40 +0200

    fit: change myenv

--- list-versions.py
+++ list-versions.py
@@ -15,7 +15,7 @@ def get_package_versions(owner, repo, package_name):
     # api_url = f"https://api.github.com/repos/{owner}/{repo}/packages/container/{package_name}/versions"
     # api_url = f"https://api.github.com/{owner}/{repo}/pkgs/container/{package_name}/versions"
 
-    api_url = f"https://api.github.com/zoeleve/Test/pkgs/container/main-temp/versions"
+    api_url = f"https://api.github.com/zoeleve/Test/packages/container/main-temp/versions"
 
     headers = {
         "Authorization": f"Bearer {token}",
